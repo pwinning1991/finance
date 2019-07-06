ed93cdd227c0d3a38ecbfe396261dbd4cb5b04d7
Philip Winnington <ftcpjw@yahoo.com>
2019-07-06 19:17:42 -0400

    added new file

---
+++ section1/5.py
@@ -0,0 +1,8 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+style.use('ggplot')
+
+df = pd.read_csv('tesla.csv', parse_dates=True, index_col=0)
